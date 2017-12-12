9e3b4681e23ddb0c998ff4c698964bb7c111f52a
Kazunari Sekigawa <sekigawa@titan.ocn.ne.jp>
2017-12-12 18:22:34 +0900

    To modify Bash script for Yosemite.
    This refs #1.

--- macbuild/macbuildQt4-yosemite.sh
+++ macbuild/macbuildQt4-yosemite.sh
@@ -7,15 +7,15 @@
 ./build.sh \
     -release \
     -qmake          /opt/local/libexec/qt4/bin/qmake \
-    -build          ./qt4.build.macos-high-sierra \
-    -bin            ./qt4.bin.macos-high-sierra \
-    -option         -j2 \
+    -build          ./qt4.build.macos-yosemite \
+    -bin            ./qt4.bin.macos-yosemite \
+    -option         -j4 \
     -with-qtbinding \
     -qt4 \
-    -ruby           /System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/ruby \
+    -ruby           /System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/bin/ruby \
     -python         /System/Library/Frameworks/Python.framework/Versions/2.7/bin/python \
     -rbinc          /System/Library/Frameworks/Ruby.framework/Headers \
-    -rblib          /System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/lib/libruby.dylib \
+    -rblib          /System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/lib/libruby.dylib \
     -pyinc          /System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
     -pylib          /System/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib 2>&1 \
     | tee macbuildQt4.log

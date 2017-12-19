0f14c138604e23dc95320f57f1497d1f97242756
Kazunari Sekigawa <sekigawa@titan.ocn.ne.jp>
2017-12-19 19:36:54 +0900

    To try suggested patch "patch0.patch.txt" provided by Matthias.
    This refs #8.

--- src/tl/tl/tlEventsVar.h
+++ src/tl/tl/tlEventsVar.h
@@ -52,6 +52,12 @@ public:
     //  .. nothing yet ..
   }
 
+  event_function (const event_function &other)
+    : m_m (other.m_m)
+  {
+    //  .. nothing yet ..
+  }
+
   virtual void call (_JOIN(tl::Object *object, _CALLARGLIST))
   {
     T *t = dynamic_cast<T *> (object);
@@ -77,6 +83,12 @@ class event_function_with_data<T, D, _TMPLARGLISTP>
 public:
   event_function_with_data (void (T::*m) (_JOIN(D, _TMPLARGLIST)), D d)
     : m_m (m), m_d (d)
+  {
+    //  .. nothing yet ..
+  }
+
+  event_function_with_data (const event_function_with_data &other)
+    : m_m (other.m_m), m_d (other.m_d)
   {
     //  .. nothing yet ..
   }
@@ -111,6 +123,12 @@ public:
     //  .. nothing yet ..
   }
 
+  generic_event_function (const generic_event_function &other)
+    : m_m (other.m_m)
+  {
+    //  .. nothing yet ..
+  }
+
   virtual void call (_JOIN(tl::Object *object, _CALLARGLIST))
   {
     T *t = dynamic_cast<T *> (object);
@@ -137,6 +155,12 @@ class generic_event_function_with_data<T, D, _TMPLARGLISTP>
 public:
   generic_event_function_with_data (void (T::*m) (D, int, void **), D d)
     : m_m (m), m_d (d)
+  {
+    //  .. nothing yet ..
+  }
+
+  generic_event_function_with_data (const generic_event_function_with_data &other)
+    : m_m (other.m_m), m_d (other.m_d)
   {
     //  .. nothing yet ..
   }

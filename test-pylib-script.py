a834fd281d4a0ab898967aafa1c1dd1431355047
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-07-11 19:43:07 -0400

    Deploying tarballs

--- test-pylib-script.py
+++ test-pylib-script.py
@@ -1,11 +1,5 @@
 import site; print(site.getsitepackages())
 
-import pip
-installed_packages = pip.get_installed_distributions()
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-     for i in installed_packages])
-print(installed_packages_list)
-
 def get_pip_main():
     import pip
     # check if pip version is new:
@@ -14,20 +8,6 @@ def get_pip_main():
     else:
         from pip import main
     return main
- 
-print("-------------")
-
-pipmain = get_pip_main()
-
-if pipmain(['install', '--upgrade', 'numpy']) > 0:
-    exit(1)
-print("-------------")
-
-
-print("Importing numpy")
-import numpy
-print("-------------")
-
 
 import sys
 print("Executing from: ", sys.executable)
@@ -37,3 +17,14 @@ import os
 print("Environment variables:")
 for variable, value in os.environ.items():
     print(variable, ":", value)
+
+pipmain = get_pip_main()
+print("-------------")
+if pipmain(['install', '--upgrade', 'numpy']) > 0:
+    exit(1)
+print("-------------")
+
+
+print("Importing numpy")
+import numpy
+print("-------------")

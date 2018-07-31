0945f7c914a20dab28b16ab49cd31ac4717351ed
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-07-31 18:44:04 -0400

    using line-buffering option in sed filter for travis CI
    tip from:
    https://stackoverflow.com/questions/49661118/lengthy-piped-command-output-hangs-when-passed-to-sed-or-awk

--- filter-clang.sh
+++ filter-clang.sh
@@ -1,3 +1,3 @@
 filter () { 
-    sed -E 's/^\/Applications\/Xcode.app\/Contents\/Developer\/Toolchains\/XcodeDefault\.xctoolchain\/usr\/bin\/clang\+\+ -c[[:space:]]+([^[:space:]]+[[:space:]]+)*/Compiling \1/' 
+    sed -lE 's/^\/Applications\/Xcode.app\/Contents\/Developer\/Toolchains\/XcodeDefault\.xctoolchain\/usr\/bin\/clang\+\+ -c[[:space:]]+([^[:space:]]+[[:space:]]+)*/Compiling \1/' 
 }

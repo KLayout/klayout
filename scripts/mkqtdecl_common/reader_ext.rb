b0d7f5f7f53fec4349148e6294c4fd0a92b72058
Matthias Koefferlein <matthias@koefferlein.de>
2019-01-08 00:58:45 +0100

    Updated copyright.

--- scripts/mkqtdecl_common/reader_ext.rb
+++ scripts/mkqtdecl_common/reader_ext.rb
@@ -1,6 +1,6 @@
 
 # 
-# Copyright (C) 2006-2018 Matthias Koefferlein
+# Copyright (C) 2006-2019 Matthias Koefferlein
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

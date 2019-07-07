bc2d9448d6f98ba8d58c4c60a0f83006124f9efd
Matthias Koefferlein <matthias@koefferlein.de>
2019-07-07 21:33:28 +0200

    Providing LVS tests.

--- src/lvs/unit_tests/lvsSimpleTests.cc
+++ src/lvs/unit_tests/lvsSimpleTests.cc
@@ -28,64 +28,36 @@
 #include "lymMacro.h"
 #include "tlFileUtils.h"
 
-#if 0 // @@@
-TEST(1)
+void run_test (tl::TestBase *_this, const std::string &suffix, const std::string &layout, bool with_l2n = false)
 {
   std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_1.lvs";
+  rs += "/testdata/lvs/" + suffix + ".lvs";
 
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au1.gds";
+  std::string src = tl::testsrc ();
+  src += "/testdata/lvs/" + layout;
 
-  std::string output = this->tmp_file ("tmp.gds");
+  std::string au_lvsdb = tl::testsrc ();
+  au_lvsdb += "/testdata/lvs/" + suffix + ".lvsdb";
 
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = nil\n"
-        "$lvs_test_target = '%s'\n"
-      , output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
+  std::string au_cir = tl::testsrc ();
+  au_cir += "/testdata/lvs/" + suffix + ".cir";
 
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
+  std::string au_l2n = tl::testsrc ();
+  au_l2n += "/testdata/lvs/" + suffix + ".l2n";
 
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
-}
-
-TEST(2)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_2.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/lvstest.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au2.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
+  std::string output_lvsdb = _this->tmp_file ("tmp.lvsdb");
+  std::string output_cir = _this->tmp_file ("tmp.cir");
+  std::string output_l2n = _this->tmp_file ("tmp.l2n");
 
   {
     //  Set some variables
     lym::Macro config;
     config.set_text (tl::sprintf (
         "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
+        "$lvs_test_target_lvsdb = '%s'\n"
+        "$lvs_test_target_cir = '%s'\n"
+        "$lvs_test_target_l2n = '%s'\n"
+      , src, output_lvsdb, output_cir, output_l2n)
     );
     config.set_interpreter (lym::Macro::Ruby);
     EXPECT_EQ (config.run (), 0);
@@ -95,529 +67,30 @@ TEST(2)
   lvs.load_from (rs);
   EXPECT_EQ (lvs.run (), 0);
 
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
+  _this->compare_text_files (output_lvsdb, au_lvsdb);
+  _this->compare_text_files (output_cir, au_cir);
+  if (with_l2n) {
+    _this->compare_text_files (output_l2n, au_l2n);
   }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
 }
 
-TEST(3_Flat)
+TEST(1_simple)
 {
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_3.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/lvstest.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au3.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
+  run_test (_this, "ringo_simple", "ringo.gds");
 }
 
-TEST(4_Hierarchical)
+TEST(2_simple_io)
 {
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_4.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/lvstest.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au4.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
+  run_test (_this, "ringo_simple_io", "ringo.gds");
 }
 
-TEST(5_FlatAntenna)
+TEST(3_simple_io2)
 {
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_5.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/antenna_l1.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au5.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
+  run_test (_this, "ringo_simple_io2", "ringo.gds", true);
 }
 
-TEST(6_HierarchicalAntenna)
+TEST(4_simple_implicit_connections)
 {
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_6.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/antenna_l1.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au6.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
+  run_test (_this, "ringo_simple_implicit_connections", "ringo_implicit_connections.gds");
 }
 
-TEST(7_AntennaWithDiodes)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_7.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/antenna_l1.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au7.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
-}
-
-TEST(8_TextsAndPolygons)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_8.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/texts.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au8.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
-}
-
-static void compare_netlists (tl::TestBase *_this, const std::string &cir, const std::string &cir_au)
-{
-  db::Netlist nl, nl_au;
-
-  db::NetlistSpiceReader reader;
-
-  {
-    tl::info << "Output: " << cir;
-    tl::InputStream is (cir);
-    reader.read (is, nl);
-  }
-
-  {
-    tl::info << "Golden: " << cir_au;
-    tl::InputStream is (cir_au);
-    reader.read (is, nl_au);
-  }
-
-  db::compare_netlist (_this, nl, nl_au);
-}
-
-TEST(9_NetlistExtraction)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_9.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/ringo.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au9a.cir";
-
-  std::string au_simplified = tl::testsrc ();
-  au_simplified += "/testdata/lvs/lvsSimpleTests_au9b.cir";
-
-  std::string output = this->tmp_file ("tmp.cir");
-  std::string output_simplified = this->tmp_file ("tmp_simplified.cir");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-        "$lvs_test_target_simplified = '%s'\n"
-      , input, output, output_simplified)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-
-  //  verify
-
-  CHECKPOINT ();
-  compare_netlists (_this, output, au);
-
-  CHECKPOINT ();
-  compare_netlists (_this, output_simplified, au_simplified);
-}
-
-TEST(10_NetlistExtractionFlat)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_10.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/ringo.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au10a.cir";
-
-  std::string au_simplified = tl::testsrc ();
-  au_simplified += "/testdata/lvs/lvsSimpleTests_au10b.cir";
-
-  std::string output = this->tmp_file ("tmp.cir");
-  std::string output_simplified = this->tmp_file ("tmp_simplified.cir");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-        "$lvs_test_target_simplified = '%s'\n"
-      , input, output, output_simplified)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-
-  //  verify
-
-  CHECKPOINT ();
-  compare_netlists (_this, output, au);
-
-  CHECKPOINT ();
-  compare_netlists (_this, output_simplified, au_simplified);
-}
-
-TEST(11_CustomDevices)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_11.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/vdiv.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au11a.cir";
-
-  std::string au_simplified = tl::testsrc ();
-  au_simplified += "/testdata/lvs/lvsSimpleTests_au11b.cir";
-
-  std::string output = this->tmp_file ("tmp.cir");
-  std::string output_simplified = this->tmp_file ("tmp_simplified.cir");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-        "$lvs_test_target_simplified = '%s'\n"
-      , input, output, output_simplified)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-
-  //  verify
-
-  CHECKPOINT ();
-  compare_netlists (_this, output, au);
-
-  CHECKPOINT ();
-  compare_netlists (_this, output_simplified, au_simplified);
-}
-
-TEST(12_NetlistJoinLabels)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_12.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/implicit_nets.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au12a.cir";
-
-  std::string output = this->tmp_file ("tmp.cir");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-        "$lvs_test_target_simplified = nil\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  //  verify
-
-  CHECKPOINT ();
-  compare_netlists (_this, output, au);
-}
-
-TEST(13a_KissingCorners)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_13a.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/kissing_corners.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au13a.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  //  verify
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  CHECKPOINT ();
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
-}
-
-TEST(13b_KissingCornersDeep)
-{
-  std::string rs = tl::testsrc ();
-  rs += "/testdata/lvs/lvsSimpleTests_13b.lvs";
-
-  std::string input = tl::testsrc ();
-  input += "/testdata/lvs/kissing_corners.gds";
-
-  std::string au = tl::testsrc ();
-  au += "/testdata/lvs/lvsSimpleTests_au13b.gds";
-
-  std::string output = this->tmp_file ("tmp.gds");
-
-  {
-    //  Set some variables
-    lym::Macro config;
-    config.set_text (tl::sprintf (
-        "$lvs_test_source = '%s'\n"
-        "$lvs_test_target = '%s'\n"
-      , input, output)
-    );
-    config.set_interpreter (lym::Macro::Ruby);
-    EXPECT_EQ (config.run (), 0);
-  }
-
-  lym::Macro lvs;
-  lvs.load_from (rs);
-  EXPECT_EQ (lvs.run (), 0);
-
-  //  verify
-
-  db::Layout layout;
-
-  {
-    tl::InputStream stream (output);
-    db::Reader reader (stream);
-    reader.read (layout);
-  }
-
-  CHECKPOINT ();
-  db::compare_layouts (_this, layout, au, db::NoNormalization);
-}
-#endif

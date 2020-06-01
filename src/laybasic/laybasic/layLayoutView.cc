378c18c3737bb64e4e5356048fa21af82efc81b7
Matthias Koefferlein <matthias@koefferlein.de>
2020-06-01 10:20:11 +0200

    PORT BACK: fixed a few flaws (fixed-width side panel ..)
    1. On "save as" the filename displayed in the cell view selection box
       was not updated
    2. The width of the library and cellview panel could not be reduced
       below the width of the combo boxes in the headers. So the
       panels might have become pretty wide without being able to reduce
       them.

--- src/laybasic/laybasic/layLayoutView.cc
+++ src/laybasic/laybasic/layLayoutView.cc
@@ -4739,6 +4739,8 @@ LayoutView::active_library_changed (int /*index*/)
 void
 LayoutView::cellview_changed (unsigned int index)
 {
+  mp_hierarchy_panel->do_update_content (index);
+
   cellview_changed_event (index);
 
   if (m_title.empty ()) {

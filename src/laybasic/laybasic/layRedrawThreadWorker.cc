aa97d4abe12e48ba3b5a05a2e46a8e6beb9351e9
Matthias Koefferlein <matthias@koefferlein.de>
2020-11-21 23:38:50 +0100

    Fixed display bug happening when child cells are instantiated with a small magnification factor.

--- src/laybasic/laybasic/layRedrawThreadWorker.cc
+++ src/laybasic/laybasic/layRedrawThreadWorker.cc
@@ -31,6 +31,15 @@ namespace lay
 const int first_snapshot_delay = 20;
 
 // -------------------------------------------------------------
+
+static inline db::Box safe_transformed_box (const db::Box &box, const db::ICplxTrans &t)
+{
+  db::DBox db = db::CplxTrans (t) * box;
+  db &= db::DBox (db::Box::world ());
+  return db::Box (db);
+}
+
+// -------------------------------------------------------------
 //  RedrawThreadWorker implementation 
 
 RedrawThreadWorker::RedrawThreadWorker (RedrawThread *redraw_thread)
@@ -815,7 +824,7 @@ RedrawThreadWorker::draw_boxes (bool drawing_context, db::cell_index_type ci, co
 
                   test_snapshot (0);
                   db::ICplxTrans t (cell_inst.complex_trans (*p));
-                  db::Box new_vp = db::Box (t.inverted () * *v);
+                  db::Box new_vp = safe_transformed_box (*v, t.inverted ());
                   draw_boxes (drawing_context, new_ci, trans * t, new_vp, level + 1);
 
                 }
@@ -974,7 +983,7 @@ RedrawThreadWorker::draw_box_properties (bool drawing_context, db::cell_index_ty
               
                   test_snapshot (0); 
                   db::ICplxTrans t (cell_inst.complex_trans (*p));
-                  db::Box new_vp = db::Box (t.inverted () * *v);
+                  db::Box new_vp = safe_transformed_box (*v, t.inverted ());
                   draw_box_properties (drawing_context, new_ci, trans * t, new_vp, level + 1, cell_inst_prop);
 
                 }
@@ -1459,7 +1468,7 @@ RedrawThreadWorker::draw_text_layer (bool drawing_context, db::cell_index_type c
                         p.index_a () <= 0 || (unsigned long)p.index_a () == amax - 1 || p.index_b () <= 0 || (unsigned long)p.index_b () == bmax - 1) {
 
                       db::ICplxTrans t (cell_inst.complex_trans (*p));
-                      db::Box new_vp = db::Box (t.inverted () * *v);
+                      db::Box new_vp = safe_transformed_box (*v, t.inverted ());
                       draw_text_layer (drawing_context, new_ci, trans * t, new_vp, level + 1, fill, frame, vertex, text, opt_bitmap);
 
                     } 
@@ -1733,7 +1742,7 @@ RedrawThreadWorker::draw_layer_wo_cache (int from_level, int to_level, db::cell_
                     p.index_a () <= 0 || (unsigned long)p.index_a () == amax - 1 || p.index_b () <= 0 || (unsigned long)p.index_b () == bmax - 1) {
 
                   db::ICplxTrans t (cell_inst.complex_trans (*p));
-                  db::Box new_vp = db::Box (t.inverted () * *v);
+                  db::Box new_vp = safe_transformed_box (*v, t.inverted ());
                   draw_layer (from_level, to_level, new_ci, trans * t, new_vp, level + 1, fill, frame, vertex, text, update_snapshot);
 
                 } 

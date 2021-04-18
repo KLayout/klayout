b1943c8b4ef782bcce56730be71a6a51ea79b2e1
Matthias Koefferlein <matthias@koefferlein.de>
2021-04-18 10:43:34 +0200

    Attempt to mitigate the effects of bpo-36085 in Python 3.8+ on Windows - need to add DLL search paths explicitly.

--- testdata/pymod/import_QtCore.py
+++ testdata/pymod/import_QtCore.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-
+import testprep
 import klayout.QtCore as QtCore
 import unittest
 import sys

3ce7038a7dd95d3365257cc0af4e296c360dd712
Matthias Koefferlein <matthias@koefferlein.de>
2021-11-10 23:38:33 +0100

    Alternative fix using a customized implementation for QMessageLogger functions

--- src/gsiqt/qt5/QtCore/gsiDeclQMessageLogger.cc
+++ src/gsiqt/qt5/QtCore/gsiDeclQMessageLogger.cc
@@ -38,6 +38,36 @@
 
 // -----------------------------------------------------------------------
 // class QMessageLogger
+  static void critical1(QMessageLogger *logger, const char *msg) {
+    logger->critical("%s", msg);
+  }
+  static void critical2(QMessageLogger *logger, const QLoggingCategory &cat, const char *msg) {
+    logger->critical(cat, "%s", msg);
+  }
+  static void debug1(QMessageLogger *logger, const char *msg) {
+    logger->debug("%s", msg);
+  }
+  static void debug2(QMessageLogger *logger, const QLoggingCategory &cat, const char *msg) {
+    logger->debug(cat, "%s", msg);
+  }
+  static void fatal1(QMessageLogger *logger, const char *msg) {
+    logger->fatal("%s", msg);
+  }
+  static void info1(QMessageLogger *logger, const char *msg) {
+    logger->info("%s", msg);
+  }
+  static void info2(QMessageLogger *logger, const QLoggingCategory &cat, const char *msg) {
+    logger->info(cat, "%s", msg);
+  }
+  static void noDebug1(QMessageLogger *logger, const char *msg) {
+    logger->noDebug("%s", msg);
+  }
+  static void warning1(QMessageLogger *logger, const char *msg) {
+    logger->warning("%s", msg);
+  }
+  static void warning2(QMessageLogger *logger, const QLoggingCategory &cat, const char *msg) {
+    logger->warning(cat, "%s", msg);
+  }
 
 //  Constructor QMessageLogger::QMessageLogger()
 
@@ -107,49 +137,6 @@ static void _call_ctor_QMessageLogger_5636 (const qt_gsi::GenericStaticMethod * 
 }
 
 
-// void QMessageLogger::critical(const char *msg, ...)
-
-
-static void _init_f_critical_c1731 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("msg");
-  decl->add_arg<const char * > (argspec_0);
-  decl->set_return<void > ();
-}
-
-static void _call_f_critical_c1731 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const char *arg1 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->critical (arg1);
-}
-
-
-// void QMessageLogger::critical(const QLoggingCategory &cat, const char *msg, ...)
-
-
-static void _init_f_critical_c4558 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("cat");
-  decl->add_arg<const QLoggingCategory & > (argspec_0);
-  static gsi::ArgSpecBase argspec_1 ("msg");
-  decl->add_arg<const char * > (argspec_1);
-  decl->set_return<void > ();
-}
-
-static void _call_f_critical_c4558 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const QLoggingCategory &arg1 = gsi::arg_reader<const QLoggingCategory & >() (args, heap);
-  const char *arg2 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->critical (arg1, arg2);
-}
-
-
 // QDebug QMessageLogger::critical()
 
 
@@ -181,49 +168,6 @@ static void _call_f_critical_c2935 (const qt_gsi::GenericMethod * /*decl*/, void
   tl::Heap heap;
   const QLoggingCategory &arg1 = gsi::arg_reader<const QLoggingCategory & >() (args, heap);
   ret.write<QDebug > ((QDebug)((QMessageLogger *)cls)->critical (arg1));
-}
-
-
-// void QMessageLogger::debug(const char *msg, ...)
-
-
-static void _init_f_debug_c1731 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("msg");
-  decl->add_arg<const char * > (argspec_0);
-  decl->set_return<void > ();
-}
-
-static void _call_f_debug_c1731 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const char *arg1 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->debug (arg1);
-}
-
-
-// void QMessageLogger::debug(const QLoggingCategory &cat, const char *msg, ...)
-
-
-static void _init_f_debug_c4558 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("cat");
-  decl->add_arg<const QLoggingCategory & > (argspec_0);
-  static gsi::ArgSpecBase argspec_1 ("msg");
-  decl->add_arg<const char * > (argspec_1);
-  decl->set_return<void > ();
-}
-
-static void _call_f_debug_c4558 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const QLoggingCategory &arg1 = gsi::arg_reader<const QLoggingCategory & >() (args, heap);
-  const char *arg2 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->debug (arg1, arg2);
 }
 
 
@@ -261,69 +205,6 @@ static void _call_f_debug_c2935 (const qt_gsi::GenericMethod * /*decl*/, void *c
 }
 
 
-// void QMessageLogger::fatal(const char *msg, ...)
-
-
-static void _init_f_fatal_c1731 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("msg");
-  decl->add_arg<const char * > (argspec_0);
-  decl->set_return<void > ();
-}
-
-static void _call_f_fatal_c1731 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const char *arg1 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->fatal (arg1);
-}
-
-
-// void QMessageLogger::info(const char *msg, ...)
-
-
-static void _init_f_info_c1731 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("msg");
-  decl->add_arg<const char * > (argspec_0);
-  decl->set_return<void > ();
-}
-
-static void _call_f_info_c1731 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const char *arg1 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->info (arg1);
-}
-
-
-// void QMessageLogger::info(const QLoggingCategory &cat, const char *msg, ...)
-
-
-static void _init_f_info_c4558 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("cat");
-  decl->add_arg<const QLoggingCategory & > (argspec_0);
-  static gsi::ArgSpecBase argspec_1 ("msg");
-  decl->add_arg<const char * > (argspec_1);
-  decl->set_return<void > ();
-}
-
-static void _call_f_info_c4558 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const QLoggingCategory &arg1 = gsi::arg_reader<const QLoggingCategory & >() (args, heap);
-  const char *arg2 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->info (arg1, arg2);
-}
-
-
 // QDebug QMessageLogger::info()
 
 
@@ -358,26 +239,6 @@ static void _call_f_info_c2935 (const qt_gsi::GenericMethod * /*decl*/, void *cl
 }
 
 
-// void QMessageLogger::noDebug(const char *, ...)
-
-
-static void _init_f_noDebug_c1731 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("arg1");
-  decl->add_arg<const char * > (argspec_0);
-  decl->set_return<void > ();
-}
-
-static void _call_f_noDebug_c1731 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const char *arg1 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->noDebug (arg1);
-}
-
-
 // QNoDebug QMessageLogger::noDebug()
 
 
@@ -390,49 +251,6 @@ static void _call_f_noDebug_c0 (const qt_gsi::GenericMethod * /*decl*/, void *cl
 {
   __SUPPRESS_UNUSED_WARNING(args);
   ret.write<QNoDebug > ((QNoDebug)((QMessageLogger *)cls)->noDebug ());
-}
-
-
-// void QMessageLogger::warning(const char *msg, ...)
-
-
-static void _init_f_warning_c1731 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("msg");
-  decl->add_arg<const char * > (argspec_0);
-  decl->set_return<void > ();
-}
-
-static void _call_f_warning_c1731 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const char *arg1 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->warning (arg1);
-}
-
-
-// void QMessageLogger::warning(const QLoggingCategory &cat, const char *msg, ...)
-
-
-static void _init_f_warning_c4558 (qt_gsi::GenericMethod *decl)
-{
-  static gsi::ArgSpecBase argspec_0 ("cat");
-  decl->add_arg<const QLoggingCategory & > (argspec_0);
-  static gsi::ArgSpecBase argspec_1 ("msg");
-  decl->add_arg<const char * > (argspec_1);
-  decl->set_return<void > ();
-}
-
-static void _call_f_warning_c4558 (const qt_gsi::GenericMethod * /*decl*/, void *cls, gsi::SerialArgs &args, gsi::SerialArgs &ret) 
-{
-  __SUPPRESS_UNUSED_WARNING(args);
-  tl::Heap heap;
-  const QLoggingCategory &arg1 = gsi::arg_reader<const QLoggingCategory & >() (args, heap);
-  const char *arg2 = gsi::arg_reader<const char * >() (args, heap);
-  __SUPPRESS_UNUSED_WARNING(ret);
-  ((QMessageLogger *)cls)->warning (arg1, arg2);
 }
 
 
@@ -479,29 +297,30 @@ static gsi::Methods methods_QMessageLogger () {
   methods += new qt_gsi::GenericStaticMethod ("new", "@brief Constructor QMessageLogger::QMessageLogger()\nThis method creates an object of class QMessageLogger.", &_init_ctor_QMessageLogger_0, &_call_ctor_QMessageLogger_0);
   methods += new qt_gsi::GenericStaticMethod ("new", "@brief Constructor QMessageLogger::QMessageLogger(const char *file, int line, const char *function)\nThis method creates an object of class QMessageLogger.", &_init_ctor_QMessageLogger_4013, &_call_ctor_QMessageLogger_4013);
   methods += new qt_gsi::GenericStaticMethod ("new", "@brief Constructor QMessageLogger::QMessageLogger(const char *file, int line, const char *function, const char *category)\nThis method creates an object of class QMessageLogger.", &_init_ctor_QMessageLogger_5636, &_call_ctor_QMessageLogger_5636);
-  methods += new qt_gsi::GenericMethod ("critical", "@brief Method void QMessageLogger::critical(const char *msg, ...)\n", true, &_init_f_critical_c1731, &_call_f_critical_c1731);
-  methods += new qt_gsi::GenericMethod ("critical", "@brief Method void QMessageLogger::critical(const QLoggingCategory &cat, const char *msg, ...)\n", true, &_init_f_critical_c4558, &_call_f_critical_c4558);
   methods += new qt_gsi::GenericMethod ("critical", "@brief Method QDebug QMessageLogger::critical()\n", true, &_init_f_critical_c0, &_call_f_critical_c0);
   methods += new qt_gsi::GenericMethod ("critical", "@brief Method QDebug QMessageLogger::critical(const QLoggingCategory &cat)\n", true, &_init_f_critical_c2935, &_call_f_critical_c2935);
-  methods += new qt_gsi::GenericMethod ("debug", "@brief Method void QMessageLogger::debug(const char *msg, ...)\n", true, &_init_f_debug_c1731, &_call_f_debug_c1731);
-  methods += new qt_gsi::GenericMethod ("debug", "@brief Method void QMessageLogger::debug(const QLoggingCategory &cat, const char *msg, ...)\n", true, &_init_f_debug_c4558, &_call_f_debug_c4558);
   methods += new qt_gsi::GenericMethod ("debug", "@brief Method QDebug QMessageLogger::debug()\n", true, &_init_f_debug_c0, &_call_f_debug_c0);
   methods += new qt_gsi::GenericMethod ("debug", "@brief Method QDebug QMessageLogger::debug(const QLoggingCategory &cat)\n", true, &_init_f_debug_c2935, &_call_f_debug_c2935);
-  methods += new qt_gsi::GenericMethod ("fatal", "@brief Method void QMessageLogger::fatal(const char *msg, ...)\n", true, &_init_f_fatal_c1731, &_call_f_fatal_c1731);
-  methods += new qt_gsi::GenericMethod ("info", "@brief Method void QMessageLogger::info(const char *msg, ...)\n", true, &_init_f_info_c1731, &_call_f_info_c1731);
-  methods += new qt_gsi::GenericMethod ("info", "@brief Method void QMessageLogger::info(const QLoggingCategory &cat, const char *msg, ...)\n", true, &_init_f_info_c4558, &_call_f_info_c4558);
   methods += new qt_gsi::GenericMethod ("info", "@brief Method QDebug QMessageLogger::info()\n", true, &_init_f_info_c0, &_call_f_info_c0);
   methods += new qt_gsi::GenericMethod ("info", "@brief Method QDebug QMessageLogger::info(const QLoggingCategory &cat)\n", true, &_init_f_info_c2935, &_call_f_info_c2935);
-  methods += new qt_gsi::GenericMethod ("noDebug", "@brief Method void QMessageLogger::noDebug(const char *, ...)\n", true, &_init_f_noDebug_c1731, &_call_f_noDebug_c1731);
   methods += new qt_gsi::GenericMethod ("noDebug", "@brief Method QNoDebug QMessageLogger::noDebug()\n", true, &_init_f_noDebug_c0, &_call_f_noDebug_c0);
-  methods += new qt_gsi::GenericMethod ("warning", "@brief Method void QMessageLogger::warning(const char *msg, ...)\n", true, &_init_f_warning_c1731, &_call_f_warning_c1731);
-  methods += new qt_gsi::GenericMethod ("warning", "@brief Method void QMessageLogger::warning(const QLoggingCategory &cat, const char *msg, ...)\n", true, &_init_f_warning_c4558, &_call_f_warning_c4558);
   methods += new qt_gsi::GenericMethod ("warning", "@brief Method QDebug QMessageLogger::warning()\n", true, &_init_f_warning_c0, &_call_f_warning_c0);
   methods += new qt_gsi::GenericMethod ("warning", "@brief Method QDebug QMessageLogger::warning(const QLoggingCategory &cat)\n", true, &_init_f_warning_c2935, &_call_f_warning_c2935);
   return methods;
 }
 
 gsi::Class<QMessageLogger> decl_QMessageLogger ("QtCore", "QMessageLogger",
+  gsi::method_ext("critical", &critical1, "@brief Method void QMessageLogger::critical(const char *msg)") +
+  gsi::method_ext("critical", &critical2, "@brief Method void QMessageLogger::critical(const QLoggingCategory &cat, const char *msg)") +
+  gsi::method_ext("debug", &debug1, "@brief Method void QMessageLogger::debug(const char *msg)") +
+  gsi::method_ext("debug", &debug2, "@brief Method void QMessageLogger::debug(const QLoggingCategory &cat, const char *msg)") +
+  gsi::method_ext("fatal", &fatal1, "@brief Method void QMessageLogger::fatal(const char *msg)") +
+  gsi::method_ext("info", &info1, "@brief Method void QMessageLogger::info(const char *msg)") +
+  gsi::method_ext("info", &info2, "@brief Method void QMessageLogger::info(const QLoggingCategory &cat, const char *msg)") +
+  gsi::method_ext("noDebug", &noDebug1, "@brief Method void QMessageLogger::noDebug(const char *msg)") +
+  gsi::method_ext("warning", &warning1, "@brief Method void QMessageLogger::warning(const char *msg)") +
+  gsi::method_ext("warning", &warning2, "@brief Method void QMessageLogger::warning(const QLoggingCategory &cat, const char *msg)")
++
   methods_QMessageLogger (),
   "@qt\n@brief Binding of QMessageLogger");
 

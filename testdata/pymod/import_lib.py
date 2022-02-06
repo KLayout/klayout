989daa82c176ea150aa2bf85892295b86f3a3a77
Matthias Koefferlein <matthias@koefferlein.de>
2022-02-06 23:28:51 +0100

    Added a test case for klayout.lib

---
+++ testdata/pymod/import_lib.py
@@ -0,0 +1,44 @@
+# KLayout Layout Viewer
+# Copyright (C) 2006-2022 Matthias Koefferlein
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import testprep
+import klayout.db as db
+import klayout.lib as lib
+import unittest
+import sys
+
+# Tests the basic abilities of the module
+
+class BasicTest(unittest.TestCase):
+
+  def test_1(self):
+
+    lib = db.Library.library_by_name("Basic")
+    self.assertEqual(lib is None, False)
+
+    pcell_decl = lib.layout().pcell_declaration("TEXT")
+    self.assertEqual(pcell_decl.name(), "TEXT")
+
+# run unit tests
+if __name__ == '__main__':
+  suite = unittest.TestSuite()
+  suite = unittest.TestLoader().loadTestsFromTestCase(BasicTest)
+
+  if not unittest.TextTestRunner(verbosity = 1).run(suite).wasSuccessful():
+    sys.exit(1)
+
+

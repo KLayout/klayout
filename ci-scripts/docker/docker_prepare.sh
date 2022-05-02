7f8c27a4ebf9feb8f279c5399e75bd2aa4e31b24
Thomas Ferreira de Lima <github@tlima.me>
2022-05-02 18:09:42 -0400

    misunderstood cibuildwheel

---
+++ ci-scripts/docker/docker_prepare.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -xe
+
+# sometimes the epel server is down. retry 5 times
+for i in $(seq 1 5); do 
+    yum install -y zlib-devel curl-devel expat-devel ccache zip git && s=0 && break || s=$? && sleep 15; 
+done
+
+[ $s -eq 0 ] || exit $s
+
+# if [[ $DOCKER_IMAGE == "quay.io/pypa/manylinux2014_x86_64" ]]; then
+ln -s /usr/bin/ccache /usr/lib64/ccache/c++
+ln -s /usr/bin/ccache /usr/lib64/ccache/cc
+ln -s /usr/bin/ccache /usr/lib64/ccache/gcc
+ln -s /usr/bin/ccache /usr/lib64/ccache/g++
+export PATH="/usr/lib64/ccache/:$PATH"
+# elif [[ $DOCKER_IMAGE == "quay.io/pypa/manylinux2014_i686" ]]; then
+ln -s /usr/bin/ccache /usr/lib/ccache/c++
+ln -s /usr/bin/ccache /usr/lib/ccache/cc
+ln -s /usr/bin/ccache /usr/lib/ccache/gcc
+ln -s /usr/bin/ccache /usr/lib/ccache/g++
+export PATH="/usr/lib/ccache/:$PATH"
+# fi
+
+echo $PATH
+export CCACHE_DIR="/host/home/runner/work/klayout/klayout/.ccache"
+ccache -M 5 G  # set cache size to 5 G
+
+# Show ccache stats
+echo "Cache stats:"
+ccache -s

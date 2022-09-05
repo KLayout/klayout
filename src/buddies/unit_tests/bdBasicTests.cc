390cac87d3f0f58b846ac6cee5b14d4034a71059
Matthias Köfferlein <matthias@koefferlein.de>
2022-09-05 23:34:53 +0200

    Issue 1097 (#1148)
    * [CONSIDER MERGING] Clarification and typo fixed in DRC doc.
    
    * Implemented #1097 (discourage file compression, use CBLOCK)
    
    * A warning is printed if file compression is enabled on OASIS
    * A hint is present in the writer options
    * CBLOCK is default (with fresh config) and for buddy tools
      (use --cblock=false to disable it)
    
    * Strict mode is default too.
    
    * Updated tests

--- src/buddies/unit_tests/bdBasicTests.cc
+++ src/buddies/unit_tests/bdBasicTests.cc
@@ -58,9 +58,9 @@ TEST(1)
                    "--write-cell-properties",
                    "--write-file-properties",
                    //  OASIS
-                   "-ob",
+                   "-ob=false",
                    "-ok=9",
-                   "-ot",
+                   "-ot=false",
                    "--recompress",
                    "--subst-char=XY",
                    "--write-std-properties=2"
@@ -85,9 +85,9 @@ TEST(1)
   EXPECT_EQ (tl::to_string (stream_opt.get_option_by_name ("gds2_user_units").to_double ()), "1");
   EXPECT_EQ (stream_opt.get_option_by_name ("gds2_write_cell_properties").to_bool (), false);
   EXPECT_EQ (stream_opt.get_option_by_name ("gds2_write_file_properties").to_bool (), false);
-  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_write_cblocks").to_bool (), false);
+  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_write_cblocks").to_bool (), true);
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_compression_level").to_int (), 2);
-  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_strict_mode").to_bool (), false);
+  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_strict_mode").to_bool (), true);
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_recompress").to_bool (), false);
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_substitution_char").to_string (), "*");
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_write_std_properties_ext").to_int (), 1);
@@ -110,9 +110,9 @@ TEST(1)
   EXPECT_EQ (tl::to_string (stream_opt.get_option_by_name ("gds2_user_units").to_double ()), "2.5");
   EXPECT_EQ (stream_opt.get_option_by_name ("gds2_write_cell_properties").to_bool (), true);
   EXPECT_EQ (stream_opt.get_option_by_name ("gds2_write_file_properties").to_bool (), true);
-  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_write_cblocks").to_bool (), true);
+  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_write_cblocks").to_bool (), false);
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_compression_level").to_int (), 9);
-  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_strict_mode").to_bool (), true);
+  EXPECT_EQ (stream_opt.get_option_by_name ("oasis_strict_mode").to_bool (), false);
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_recompress").to_bool (), true);
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_substitution_char").to_string (), "X");
   EXPECT_EQ (stream_opt.get_option_by_name ("oasis_write_std_properties_ext").to_int (), 2);

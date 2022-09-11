e460314090a8d70b9507abaf2f5022e618153971
Matthias Koefferlein <matthias@koefferlein.de>
2022-09-11 22:02:10 +0200

    Deploying more SVG based icons

--- src/icons/svg/make_png.sh
+++ src/icons/svg/make_png.sh
@@ -7,11 +7,15 @@ for svg_file in *.svg; do
   png_file=${output}/${svg_file/.svg/.png}
   png_file_2x=${output}/${svg_file/.svg/@2x.png}
 
-  echo "Converting $svg_file to $png_file .."
-  inkscape -o ${png_file} ${svg_file} -d 96 -C
+  if ! [ -e ${png_file} ] || [ ${svg_file} -nt ${png_file} ]; then
+    echo "Converting $svg_file to $png_file .."
+    inkscape -o ${png_file} ${svg_file} -d 96 -C
+  fi
 
-  echo "Converting $svg_file to $png_file_2x .."
-  inkscape -o ${png_file_2x} ${svg_file} -d 192 -C
+  if ! [ -e ${png_file_2x} ] || [ ${svg_file} -nt ${png_file_2x} ]; then
+    echo "Converting $svg_file to $png_file_2x .."
+    inkscape -o ${png_file_2x} ${svg_file} -d 192 -C
+  fi
 
 done
 

b213479e07b1851fb45dd282c1f6ab20e0f34dca
klayoutmatthias <matthias@klayout.org>
2022-12-31 18:51:08 +0100

    Fixed issue #1238 (Macro IDE: breakpoints not effective on Windows)

--- src/lay/lay/layMacroEditorDialog.cc
+++ src/lay/lay/layMacroEditorDialog.cc
@@ -3079,7 +3079,7 @@ size_t
 MacroEditorDialog::id_for_path (gsi::Interpreter *, const std::string &path)
 {
   for (std::map <lym::Macro *, MacroEditorPage *>::const_iterator m = m_tab_widgets.begin (); m != m_tab_widgets.end (); ++m) {
-    if (m->first->path () == path) {
+    if (tl::is_same_file(m->first->path (), path)) {
       m_file_to_widget.push_back (*m);
       return m_file_to_widget.size ();
     }

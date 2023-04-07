9c1da8d3e18f57a778aa1e5c110b5ea64a0bc94e
Matthias Koefferlein <matthias@klayout.de>
2023-04-07 11:44:20 +0200

    Fixed issue #1328 (width of layer selection boxes too small)
    Also: clip and fill dialog now display layer names from
    the layer view. Fixed geometry of fill dialog.
    
    Hope I got all places where the layer selection box was
    configured as adjust size = on first show.

--- src/lay/lay/layFillDialog.cc
+++ src/lay/lay/layFillDialog.cc
@@ -102,10 +102,12 @@ FillDialog::menu_activated (const std::string &symbol)
 {
   if (symbol == "fill_tool::show") {
 
-    lay::CellView cv = mp_view->cellview (mp_view->active_cellview_index ());
+    int cv_index = mp_view->active_cellview_index ();
+
+    lay::CellView cv = mp_view->cellview (cv_index);
     if (cv.is_valid ()) {
-      cb_layer->set_layout (&cv->layout ());
-      fc_boundary_layer->set_layout (&cv->layout ());
+      cb_layer->set_view (mp_view, cv_index);
+      fc_boundary_layer->set_view (mp_view, cv_index);
       show ();
     }
 

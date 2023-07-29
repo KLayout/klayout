4f7da687d48064659598c5e153967947cad31509
Matthias Koefferlein <matthias@klayout.de>
2023-07-29 09:50:58 +0200

    Removed a question comment

--- src/plugins/streamers/lefdef/db_plugin/dbDEFImporter.cc
+++ src/plugins/streamers/lefdef/db_plugin/dbDEFImporter.cc
@@ -588,9 +588,6 @@ DEFImporter::read_single_net (std::string &nondefaultrule, Layout &layout, db::C
           error (tl::to_string (tr ("RECT routing specification not followed by coordinate list")));
         }
 
-        //  breaks wiring? Issue #1432 says: no.
-        //  pts.clear ();
-
         //  rect spec
 
         double x1 = get_double ();

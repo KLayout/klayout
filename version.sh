20a23d8e8a5c16fd5ef6caf8be8b41f0ce0cbc89
Matthias Koefferlein <matthias@klayout.de>
2024-03-19 00:53:55 +0100

    Enabling Python 3.12 for Windows for PyPI. Needs a new release

--- version.sh
+++ version.sh
@@ -5,7 +5,7 @@
 KLAYOUT_VERSION="0.28.17"
 
 # The version used for PyPI (don't use variables here!)
-KLAYOUT_PYPI_VERSION="0.28.17"
+KLAYOUT_PYPI_VERSION="0.28.17-1"
 
 # The build date
 KLAYOUT_VERSION_DATE=$(date "+%Y-%m-%d")

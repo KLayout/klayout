5d57cb5eed24aa72c4417e9a1a750aeb31100bb7
Matthias Koefferlein <matthias@klayout.de>
2025-01-04 19:32:33 +0100

    Updating copyright to 2025

--- scripts/mkqtdecl_common/cpp_parser_classes.rb
+++ scripts/mkqtdecl_common/cpp_parser_classes.rb
@@ -1,5 +1,5 @@
 # 
-# Copyright (C) 2006-2024 Matthias Koefferlein
+# Copyright (C) 2006-2025 Matthias Koefferlein
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 KLayout standalone Python module setup script
 
 
-    Copyright (C) 2006-2024 Matthias Koefferlein
+    Copyright (C) 2006-2025 Matthias Koefferlein
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

df86135fbf25207196868185621431bbe783312e
Matthias Koefferlein <matthias@klayout.de>
2025-08-05 23:15:57 +0200

    Preparations for release 0.30.3

--- version.sh
+++ version.sh
@@ -2,10 +2,10 @@
 # This script is sourced to define the main version parameters
 
 # The main version
-KLAYOUT_VERSION="0.30.2"
+KLAYOUT_VERSION="0.30.3"
 
 # The version used for PyPI (don't use variables here!)
-KLAYOUT_PYPI_VERSION="0.30.2"
+KLAYOUT_PYPI_VERSION="0.30.3"
 
 # The build date
 KLAYOUT_VERSION_DATE=$(date "+%Y-%m-%d")

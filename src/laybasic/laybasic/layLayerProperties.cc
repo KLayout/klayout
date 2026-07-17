edad8f52212f1bb59480d9ec37d8fd9fcb6e1382
Matthias Koefferlein <matthias@klayout.de>
2026-07-17 23:46:14 +0200

    Bugfix: update layer names in layer list (when used in secondary fashion) on reload
    1. The readers will overwrite the layer names if given in secondary fashion
       (additionally to layer/datatype)
    2. The layer properties nodes will update the "real source" according to
       the actual name if the name is used in addition to layer/datatypes

--- src/laybasic/laybasic/layLayerProperties.cc
+++ src/laybasic/laybasic/layLayerProperties.cc
@@ -640,6 +640,13 @@ LayerProperties::do_realize (const LayoutViewBase *view) const
 
       }
 
+      if (m_layer_index >= 0) {
+        const auto &lp = cv->layout ().get_properties (m_layer_index);
+        if (! lp.name.empty () && m_source_real.name () != lp.name) {
+          m_source_real.name (lp.name);
+        }
+      }
+
     }
 
   }
